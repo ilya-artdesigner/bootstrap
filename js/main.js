c246e22a3f30f6aa293426295ce0d89cbf579970
ilya fadeev <ilya-artdesigner@yandex.ru>
2020-09-28 12:58:02 +0300

    add message into btn load_more

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function(){
     $('.load_more').on('click', function() {
         const btn = $(this);
         const loader = btn.find('span');
+        const message = btn.find('i');
 
         $.ajax({
             url: '/data.html',
@@ -9,11 +10,13 @@ $(function(){
             beforeSend: function() {
                 btn.attr('disabled', true);
                 loader.addClass('d-inline-block');
+                message.text('Loading...');
             },
             success: function(responce) {
                 setTimeout(function() {
                     loader.removeClass('d-inline-block');
                     btn.attr('disabled', false);
+                    message.text('Load More');
                     $('.after_posts').before(responce);
                 }, 1000);
             },
@@ -21,6 +24,7 @@ $(function(){
                 alert('Error!');
                 loader.removeClass('d-inline-block');
                 btn.attr('disabled', false);
+                message.text('Load More');
             }
         });
     });
